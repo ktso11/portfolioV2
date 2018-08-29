64f86e392807f9d7a580fc427a86cbdabfbb5517
Katie So <katieso86@gmail.com>
2018-08-29 15:26:04 -0700

    config packagejson

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(express.static(__dirname + 'dist'));
-app.get('/*', function(req,res) {
 
-res.sendFile(path.join('/dist/index.html'));
+
+app.use(express.static(path.join(__dirname, 'dist/portfolio')));
+app.get('/*all', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/portfolio/index.html'));
 });
-
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
