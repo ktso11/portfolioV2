a89b513b3b40d84422ee562ebeb9e3abe0bef4aa
Katie So <katieso86@gmail.com>
2018-08-29 15:32:27 -0700

    config packagejson

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const app = express();
 
 
 
-app.use(express.static(path.join(__dirname + '/dist/portfolio')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/portfolio/index.html'));
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
